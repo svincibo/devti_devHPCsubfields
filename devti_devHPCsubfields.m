031cfbef2e6cd8668e304557dc427f520744dd23
Sophia Vinci-Booher <svincibo@gmail.com>
2020-03-24 14:00:00 -0400

    fixed bug

--- devti_devHPCsubfields.m
+++ devti_devHPCsubfields.m
@@ -57,10 +57,10 @@ for w = 1%:length(wm)
         end
         
         % Mean center continuous variables.
-        m = double(m - nanmean(m, 1));
+        m_demeaned = double(m - nanmean(m, 1));
         
         % Convert data to table for easier model specification.
-        data = array2table(cat(2, transpose(sub), transpose(age), transpose(sex), transpose(iq), m), 'VariableNames', {'subID', 'age', 'sex',  'iq', roi{1, :}});
+        data = array2table(cat(2, transpose(sub), transpose(age), transpose(sex), transpose(iq), m_demeaned), 'VariableNames', {'subID', 'age', 'sex',  'iq', roi{1, :}});
          
         % 1. Linear main effects model.
         modelspec = 'b_ca1 ~ sex + age + (1|subID)';
@@ -102,7 +102,7 @@ for w = 1%:length(wm)
         % Get AIC value corrected for sample size: AICc.
         disp(['AICc for ' wm{w} ' in ' subregion{r} ' using a model including nonlinear main effects is ' num2str(aicc) '.']);
         
-                % 3. Linear interaction model.
+        % 3. Linear interaction model.
         modelspec = 'b_ca1 ~ sex*age + (1|subID)';
         if sum(remove) == 0          
 
@@ -145,14 +145,73 @@ for w = 1%:length(wm)
             
     end
     
+    %% Visualize.
     
+    figure
     
+    % Correlations between age and average WM measure in ROI.
+    % CA1
+    x = transpose(age);
+    y = m(strcmp(roi, 'b_ca1'));
+    c1_color = [0 0.4470 0.7410];
+    scatter(x, y, 'filled', 'MarkerEdgeColor', c1_color, 'MarkerFaceColor', c1_color)
+    hold on;
     
+    % c1 = corr(x(~isnan(y)), y(~isnan(y)));
+    c1 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
+    x1 = linspace(0,30);
+    y1 = 1./(1+x1);
     
+    f1 = polyval(c1,x1);
+    plot(x1, f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c1_color)
+    clear x
+    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
+    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c1_color(1:3));
+    set(hp3, 'facecolor', c1_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
     
+    disp(['CA1 = ' num2str(c1)])
+    clear x y f1 hp3
     
+    % CA23dg
+    x = transpose(age);
+    y = m(strcmp(roi, 'b_ca23dg'));
+    c2_color = [0.4940 0.1840 0.5560];
+    scatter(x(~isnan(y)), y(~isnan(y)), 'filled', 'MarkerEdgeColor', c2_color, 'MarkerFaceColor', c2_color)
     
+    % c2 = corr(x(~isnan(y)), y(~isnan(y)));
+    c2 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
     
+    f1 = polyval(c2,x1);
+    plot(x1,f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c2_color)
+    clear x
+    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
+    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c2_color(1:3));
+    set(hp3, 'facecolor', c2_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
+    
+    disp(['CA23dg = ' num2str(c2)])
+    clear x y f1 hp3
+    
+    % SUB
+    x = transpose(age);
+    y = m(strcmp(roi, 'b_sub'));
+    c3_color = [0.8500 0.3250 0.0980];
+    scatter(x(~isnan(y)), y(~isnan(y)), 'filled', 'MarkerEdgeColor', c3_color, 'MarkerFaceColor', c3_color)
+    
+    % c3 = corr(x(~isnan(y)), y(~isnan(y)));
+    c3 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
+    
+    f1 = polyval(c3,x1);
+    plot(x1,f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c3_color)
+    clear x
+    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
+    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c3_color(1:3));
+    set(hp3, 'facecolor', c3_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
+    
+    disp(['SUB = ' num2str(c3)])
+    
+    legend([{'CA1'}, {''}, {''}, {'CA23dg'}, {''}, {''}, {'SUB'}, {''}], 'Location', 'southwest')
+    legend box off
+       
     if strcmp(wm{w}, 'fa')
         
         ylab = 'Fractional Anisotropy';
@@ -175,81 +234,11 @@ for w = 1%:length(wm)
         
     end
     
-    %% Visualize.
-    
-    figure(w)
-    
-    % Correlations between age and average WM measure in ROI.
-    % CA1
-    x = transpose(age);
-    y = m(strcmp(roi, 'b_ca1'));
-    c1_color = [0 0.4470 0.7410];
-    scatter(x, y, 'filled', 'MarkerEdgeColor', c1_color, 'MarkerFaceColor', c1_color)
-    hold on;
-    
-    % c1 = corr(x(~isnan(y)), y(~isnan(y)));
-    c1 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
-    x1 = linspace(0,30);
-    y1 = 1./(1+x1);
-    
-    f1 = polyval(c1,x1);
-    % plot(x1,y1)
-    plot(x1, f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c1_color)
-    clear x
-    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
-    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c1_color(1:3));
-    set(hp3, 'facecolor', c1_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
-    
-    disp(['CA1 = ' num2str(c1)])
-    clear x y f1 hp3
-    
-    % CA23dg
-    x = transpose(age);
-    y = m(strcmp(roi, 'b_ca23dg'));
-    c2_color = [0.4940 0.1840 0.5560];
-    scatter(x(~isnan(y)), y(~isnan(y)), 'filled', 'MarkerEdgeColor', c2_color, 'MarkerFaceColor', c2_color)
-    
-    % c2 = corr(x(~isnan(y)), y(~isnan(y)));
-    c2 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
-    
-    f1 = polyval(c2,x1);
-    % plot(x1,y1)
-    plot(x1,f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c2_color)
-    clear x
-    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
-    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c2_color(1:3));
-    set(hp3, 'facecolor', c2_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
-    
-    disp(['CA23dg = ' num2str(c2)])
-    clear x y f1 hp3
-    
-    % SUB
-    x = transpose(age);
-    y = m(strcmp(roi, 'b_sub'));
-    c3_color = [0.8500 0.3250 0.0980];
-    scatter(x(~isnan(y)), y(~isnan(y)), 'filled', 'MarkerEdgeColor', c3_color, 'MarkerFaceColor', c3_color)
-    
-    % c3 = corr(x(~isnan(y)), y(~isnan(y)));
-    c3 = polyfit(x(~isnan(y)), y(~isnan(y)), degp);
-    
-    f1 = polyval(c3,x1);
-    % plot(x1,y1)
-    plot(x1,f1, 'LineWidth', 3, 'LineStyle', '-', 'Color', c3_color)
-    clear x
-    hi = f1 + std(f1, 0, 2); lo = f1 - std(f1, 0, 2); x = (1:size(f1, 2))'.*.30;
-    hp3 = patch([x; x(end:-1:1); x(1)], [lo'; hi(end:-1:1)'; lo(1)], c3_color(1:3));
-    set(hp3, 'facecolor', c3_color(1:3), 'edgecolor', 'none', 'facealpha', .2);
-    
-    disp(['SUB = ' num2str(c3)])
-    
-    legend([{'CA1'}, {''}, {''}, {'CA23dg'}, {''}, {''}, {'SUB'}, {''}], 'Location', 'southwest')
-    legend box off
-    
-    ylabel(ylab)
-    xlabel('Age (years)')
-    
     xlim([0 30])
     ylim([ymin ymax])
+        
+    ylabel(ylab)
+    xlabel('Age (years)')
     
     hold off;
     
